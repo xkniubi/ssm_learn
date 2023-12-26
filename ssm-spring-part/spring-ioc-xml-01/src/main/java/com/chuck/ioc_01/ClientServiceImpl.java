8c6f550c213d28d42bd027f7dc43968d9673e85d
xiekang <1175461017@qq.com>
2023-12-26 22:34:08 +0800

    1226

---
+++ ssm-spring-part/spring-ioc-xml-01/src/main/java/com/chuck/ioc_01/ClientServiceImpl.java
@@ -0,0 +1,13 @@
+package com.chuck.ioc_01;
+
+/**
+ * ClassName: ClientServiceImpl
+ * Package: com.chuck.ioc_01
+ * Description:
+ *
+ * @Author Chuck
+ * @Create 2023/12/26 22:04
+ * @Version 1.0
+ */
+public class ClientServiceImpl {
+}
---
+++ ssm-spring-part/spring-ioc-xml-01/src/main/java/com/chuck/ioc_01/DefaultServiceLocator.java
@@ -0,0 +1,10 @@
+package com.chuck.ioc_01;
+
+public class DefaultServiceLocator {
+
+  private static ClientServiceImpl clientService = new ClientServiceImpl();
+
+  public ClientServiceImpl createClientServiceInstance() {
+    return clientService;
+  }
+}
